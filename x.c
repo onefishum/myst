f5d4f67a7b1d1f6f1c024cb6aeba958c378f6178
onefishum <onefishum@gmail.com>
2021-07-10 18:45:53 +0800

    增加左键粘贴，diff代码比较有错误，应该加上后面的代码

--- x.c
+++ x.c
@@ -692,10 +692,12 @@ brelease(XEvent *e)
 		return;
 	}
 
-	if (mouseaction(e, 1))
-		return;
-	if (e->xbutton.button == Button1)
-		mousesel(e, 1);
+	//if (mouseaction(e, 1))
+	//		return;
+	if (e->xbutton.button == Button3)
+ 		selpaste(NULL);
+ 	else if (e->xbutton.button == Button1)
+ 		mousesel(e, 1);
 }
 
 void
